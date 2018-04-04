925bf7742c355747133bb997343985962dda3e7c
MilosDejanovic <33279874+MilosDejanovic@users.noreply.github.com>
2018-04-04 22:50:33 +0200

    Add files via upload

---
+++ Can-comun_.ino
@@ -0,0 +1,197 @@
+#include <mcp_can.h>
+#include <SPI.h>
+#include <string.h>
+
+
+#define CAN_INT 2
+
+MCP_CAN CAN0(10);
+
+int msg[8];
+char tmp[64];
+char can_msg_tmp[16];
+int i=0;
+int j = 0;
+int br=0;
+int duzina;
+char niz[8];
+long unsigned int rxId;
+unsigned char len = 0;
+byte tempbyte = 0;
+int inPin = 5;
+int val=0;
+unsigned char incByte;
+
+char inData[80];
+char flightdata[80];
+int value,msg_length;
+byte checksum = 0;
+byte bytesread = 0;
+char pniz[8];
+
+
+void setup() {
+
+  Serial.begin(115200);
+  while(CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_16MHZ) != CAN_OK){
+    Serial.println("Error Initializing MCP2515");
+  }
+  pinMode(inPin,INPUT);
+
+  CAN0.setMode(MCP_NORMAL);
+
+     
+}
+
+void loop() {
+
+  while(!Serial.available());
+        
+    delay(100);
+   
+    duzina=Serial.available();
+
+    Serial.readBytes(tmp,duzina);
+
+    Serial.println(tmp);
+
+    for(i = 0; i < duzina; i++) {
+
+       if(tmp[i] == 'p') {
+        
+        br++;
+        
+       } else {
+
+        can_msg_tmp[j] = tmp[i];
+        Serial.print("Karakter je: ");
+        Serial.println(can_msg_tmp[j]);
+        Serial.println(j);
+        
+       j++;
+       }
+      
+    }
+
+    
+    Serial.print("Duzina poruke je: ");
+    Serial.println(br);
+   /* Serial.print("Poruka je: ");
+    Serial.println(can_msg_tmp);*/
+     
+ 
+    
+    
+   
+  //can_msg[0]="/0";
+  /*for(int i=0;i<8;i++){
+   //Serial.print("usao");
+   // can_msg[i]=can_msg_tmp[i];
+  if ((can_msg_tmp[i] >= '0') && (can_msg_tmp[i] <= '9')) {
+            can_msg_tmp[i] = can_msg_tmp[i] - '0';
+          } else if ((can_msg_tmp[i] >= 'A') && (can_msg_tmp[i] <= 'F')) {
+            can_msg_tmp[i] = 10 + can_msg_tmp[i] - 'A';
+          }}*/
+  /*if(((can_msg_tmp[i] & B00001111 )   == 3 )|((can_msg_tmp[i] & B00001111)  == 4)){
+      can_msg_tmp[i]=can_msg_tmp[i] <<= 3;
+      can_msg_tmp[i]=can_msg_tmp[i] | can_msg_tmp[i+1] ;
+  }
+
+
+  }*/
+      
+  
+    
+  
+   
+   //while (bytesread < 12) {      
+/* if ((can_msg_tmp[i] >= '0') && (can_msg_tmp[i] <= '9')) {
+            can_msg[i] = can_msg_tmp[i] - '0';
+          } else if ((can_msg_tmp[i] >= 'A') && (can_msg_tmp[i] <= 'F')) {
+            can_msg[i] = 10 + can_msg_tmp[i] - 'A';
+          }
+
+          // Every two hex-digits, add byte to code:
+       if (bytesread & 1 == 1) {
+            // make some space for this hex-digit by
+            // shifting the previous hex-digit with 4 bits to the left:
+            can_msg[bytesread >> 1] = (can_msg_tmp[bytesread] | (tempbyte << 4));
+
+            if (bytesread >> 1 != 5) {                // If we're at the checksum byte,
+              checksum ^= can_msg[bytesread >> 1];       // Calculate the checksum... (XOR)
+            }
+          } else {
+           pniz[bytesread] = can_msg_tmp[bytesread];                           // Store the first hex digit first...
+          };
+
+          bytesread++;                                // ready to read next digit
+        } 
+       */
+
+    
+    /*if((can_msg_tmp[i]>='0')&&(can_msg_tmp[i]<='9'))
+    {
+      
+        can_msg[i]=can_msg_tmp[i];}
+    
+    //else i((can_msg_tmp[i]>='A')&&(can_msg_tmp[i]<='F')){
+      
+        can_msg[i]=can_msg_tmp[i];}
+  
+   Serial.print(can_msg[i]);
+   }
+    */
+
+
+
+    
+   /* for(int i=0;i<8;i++){
+      Serial.print(can_msg_tmp[i],HEX);*/
+    msg_length = ParseMessage(tmp, msg);
+    
+   
+     CAN0.sendMsgBuf(0xFF,0,3,msg);
+  
+      Serial.println("message sent");
+
+    }  
+
+
+ int makeOutputValue(int br1, int br2) {
+     int partOne = convertToHexValue(br1);
+     int partTwo = convertToHexValue(br2);
+
+  return partOne * 16 + partTwo;
+}
+
+int convertToHexValue(int br) {
+
+  if(br / 10 == 3) {
+    return br % 10;
+  } else if(br / 10 == 4) {
+    return (br % 10) + 9;
+  }
+
+}
+
+int returnHex(char chr) {
+  int chr_value = (int)chr;
+
+  if(chr_value > 47 && chr_value < 58) return chr_value - 18;
+  if(chr_value > 64 && chr_value < 71) return chr_value - 24;
+  if(chr_value > 96 && chr_value < 103) return chr_value - 56;
+
+}
+
+int ParseMessage(char *string, int *msg) {
+  int i;
+  int j = 0;
+
+  for(i = 0; i < strlen(string); i+=3) {
+    msg[j] = makeOutputValue(returnHex(string[i]), returnHex(string[i+1]));
+    j++;
+  }
+
+  return j;
+
+}
