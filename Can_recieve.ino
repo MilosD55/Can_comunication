730519b7c26dc6815148929d67014e58251c52e6
MilosDejanovic <33279874+MilosDejanovic@users.noreply.github.com>
2018-04-06 00:40:36 +0200

    Update Can_recieve.ino

--- Can_recieve.ino
+++ Can_recieve.ino
@@ -1,5 +1,4 @@
-// CAN Receive Example
-//
+
 
 #include <mcp_can.h>
 #include <SPI.h>
@@ -7,43 +6,43 @@
 long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
-char msgString[128];                        // Array to store serial string
+char msgString[128];                        
 
-#define CAN0_INT 2                              // Set INT to pin 2
-MCP_CAN CAN0(10);                               // Set CS to pin 10
+#define CAN0_INT 2                              
+MCP_CAN CAN0(10);                              
 
 
 void setup()
 {
   Serial.begin(115200);
   
-  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  
   if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
     Serial.println("MCP2515 Initialized Successfully!");
   else
     Serial.println("Error Initializing MCP2515...");
   
-  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+  CAN0.setMode(MCP_NORMAL);                     
 
-  pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+  pinMode(CAN0_INT, INPUT);                            
   
   Serial.println("MCP2515 Library Receive Example...");
 }
 
 void loop()
 {
-  if(!digitalRead(CAN0_INT))                         // If CAN0_INT pin is low, read receive buffer
+  if(!digitalRead(CAN0_INT))                         
   {
-    CAN0.readMsgBuf(&rxId, &len, rxBuf);      // Read data: len = data length, buf = data byte(s)
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);      
     
-    if((rxId & 0x80000000) == 0x80000000)     // Determine if ID is standard (11 bits) or extended (29 bits)
+    if((rxId & 0x80000000) == 0x80000000)     
       sprintf(msgString, "Extended ID: 0x%.8lX  DLC: %1d  Data:", (rxId & 0x1FFFFFFF), len);
     else
       sprintf(msgString, "Standard ID: 0x%.3lX       DLC: %1d  Data:", rxId, len);
   
     Serial.print(msgString);
   
-    if((rxId & 0x40000000) == 0x40000000){    // Determine if message is a remote request frame.
+    if((rxId & 0x40000000) == 0x40000000){   
       sprintf(msgString, " REMOTE REQUEST FRAME");
       Serial.print(msgString);
     } else {
@@ -57,6 +56,4 @@ void loop()
   }
 }
 
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
+
